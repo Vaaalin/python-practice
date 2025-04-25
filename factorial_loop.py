d97dad4a06d3e3bf9f5acd9f0b337117f06c1f97
Alvin Kyalo <kyaloalviin@gmail.com>
2025-04-25 07:38:24 +0300

    the error where the retunr result was in the loop

--- factorial_loop.py
+++ factorial_loop.py
@@ -1,8 +1,9 @@
-"""it cant be for negativess ......so we have to make sure it doestnt try anything that is <0 since 0!=1 so we kinda need to start with that """
+"""it cant be for negativess ......so we have to make sure it doestnt try anything that is <0 since 0!=1 so we kinda need to start with that 
+we added a raise value to make sure we rememebrr it can not be negative"""
 def factorial_loop(n):
     if n<0:
         raise ValueError("not for negative numbers")
     result = 1
     for i in range(1,n+1):
         result*=i
-        return result
+    return result
