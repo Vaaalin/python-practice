b58e318b030d52bff2508d5c98ce6ec54441e881
Alvin Kyalo <kyaloalviin@gmail.com>
2025-04-26 06:56:48 +0300

    Add docstring explaining number checking functionality and purpose

--- number_check.py
+++ number_check.py
@@ -1,4 +1,6 @@
-'''just like in c++ we use %2 and if divisible by 2 it should do even and if it has a remainder its odd...we ask'''
+'''just like in c++ we use %2 and if divisible by 2 it should do even and if it has a remainder its odd...we ask This function checks if a number is even or odd.
+so It basically  provides additional information about the number.
+'''
 def number_check(numbers):
     if numbers %2==0:
         return "even"
